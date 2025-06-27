d4d9ad25506000b653b3db4dce725b533d73e210
lmleall <lmleall@unal.edu.co>
2025-06-27 01:11:19 -0500

    LAB-03-ingestion-de-texto-plano-lmleall

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -18,3 +18,40 @@ def pregunta_01():
 
 
     """
+    import pandas as pd
+
+    with open("files/input/clusters_report.txt", "r", encoding="utf-8") as archivo:
+        lines = archivo.readlines()
+
+    columnas = ["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]
+
+    datos = []
+    actual = None
+    contador_cluster = 1
+
+    for line in lines[4:]:
+        if line.strip():
+            if line[:4].strip().isdigit():
+                if actual:
+                    datos.append(actual)
+                cluster = contador_cluster
+                contador_cluster += 1
+                cantidad = int(line[5:18].strip())
+                porcentaje = float(line[19:33].strip().replace(",", ".").replace("%", ""))
+                palabras_clave = line[34:].strip()
+                actual = [cluster, cantidad, porcentaje, palabras_clave]
+            else:
+                if actual:
+                    actual[-1] += " " + line.strip()
+    if actual:
+        datos.append(actual)
+    df = pd.DataFrame(datos, columns=columnas)
+    df["principales_palabras_clave"] = (
+        df["principales_palabras_clave"]
+        .str.replace("\s+", " ", regex=True)
+        .str.replace(", ", ",")
+        .str.replace(",", ", ")
+        .str.strip()
+        .str.rstrip(".")
+    )
+    return df
